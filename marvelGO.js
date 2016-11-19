675507269cb513d266f14e06092046f29fb0b8cd
Alan M <Alan M>
2016-11-19 13:12:31 -0600

    API is working!

--- marvelGO.js
+++ marvelGO.js
@@ -1,26 +1,28 @@
 var key = "82df9267e06ec89e40b14eec91deacb4";
-var queryURL = "https://gateway.marvel.com:443/v1/public/characters?name=";
+var queryURL = "https://gateway.marvel.com:443/v1/public/characters?";
 var privateKey = "accae6d1b3da682be3974ffddf1adf741480562d";
 var characterName = '';
-
-/*"https://gateway.marvel.com:443/v1/public/characters?name=Spiderman&apikey=82df9267e06ec89e40b14eec91deacb4"*/
 
 
 $(document).ready(function() {
 
+
     $("button").on("click", function() {
 
+    	var currentTime = Date.now();
 
-        characterName = $("#name").val().trim();
+    	var hash = md5(currentTime + privateKey + key);
 
-        /*queryURL += characterName + "&apikey=" + key;*/
+        characterName = $("#name").val().trim().replace(" ", "%20");
+
+        queryURL += "name=" + characterName + "&ts=" + currentTime + "&apikey=" + key + "&hash=" + hash;
 
         console.log(queryURL);
 
         $.ajax({ url: queryURL, method: 'GET' }).done(function(response){
 
 
-            console.log(response.character.name);
+        	console.log(response.data.results[0].description);
 
 
         });
